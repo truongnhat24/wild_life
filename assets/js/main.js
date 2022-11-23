16e51e5665da5676b7576271bbf9217bf3c7fc77
truongnhat <truongnhat2492@gmail.com>
2022-11-23 09:44:46 +0700

    update footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,7 @@
 // Open & close header menu 
 let btnMenu = document.querySelector("header nav .btn-menu");
-let btnFooter = document.querySelector("footer .btn-footer");
 let currentMenu = document.querySelector("header nav .menu");
-let loginBtn = document.querySelector("header nav .btn-group");
-let contentFooter = document.querySelector("footer .footer-content");
 
 btnMenu.addEventListener("click", () =>{
     currentMenu.classList.toggle("open");
-    if (document.querySelector("header").clientWidth < 768 ){
-        loginBtn.classList.toggle("show");
-    }
-})
-
-btnFooter.addEventListener("click", () => {
-    contentFooter.classList.toggle("show");
 })
